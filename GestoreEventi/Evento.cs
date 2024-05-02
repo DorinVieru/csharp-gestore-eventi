340eb5578d647b7255d44c2538431e3e70da1d60
DorinVieru <dorino.vieru@gmail.com>
2024-05-02 10:32:45 +0200

    UP: Write get, set & errors of different attribute

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -23,6 +23,49 @@ namespace GestoreEventi
             this.PostiPrenotati = 0; 
         }
 
-        
+        // Gestione dei getter, dei setter e degli errori
+        public string Titolo
+        {
+            get { return titolo; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Deve esserci un titolo, non è possibile lasciare vuoto questo campo.");
+                titolo = value;
+            }
+        }
+
+        public DateTime Data
+        {
+            get { return data; }
+            set
+            {
+                if (value < DateTime.Now)
+                    throw new ArgumentOutOfRangeException("La data inserita non può essere precedente alla data corrente.");
+                data = value;
+            }
+        }
+
+        public int CapienzaMassima
+        {
+            get { return capienzaMassima; }
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("La capienza massima deve essere un numero positivo (maggiore di 0).");
+                capienzaMassima = value;
+            }
+        }
+
+        public int PostiPrenotati
+        {
+            get { return postiPrenotati; }
+            private set
+            {
+                if (value < 0 || value > CapienzaMassima)
+                    throw new ArgumentOutOfRangeException("Il numero di posti prenotati deve essere compreso tra 0 (numero positivo) e la capienza massima disponibile.");
+                postiPrenotati = value;
+            }
+        }
     }
 }
