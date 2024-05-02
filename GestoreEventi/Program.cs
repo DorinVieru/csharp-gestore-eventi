e6a3258d68a76e5bc13d5a1d1a399076535eb641
DorinVieru <dorino.vieru@gmail.com>
2024-05-02 11:11:20 +0200

    UP: Complete MILESTONE 1; write creation of event

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -4,6 +4,18 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inizia a gestire subito i tuoi eventi meravigliosi!");
+
+            // Creazione di un nuovo evento
+            Console.Write("Inserisci il nome dell'evento (obbligatorio): ");
+            string titolo = Console.ReadLine();
+            Console.Write("Inserisci la data dell'evento (obbligatorio formato dd/MM/yyyy): ");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+            Console.Write("Inserisci il numero di posti totali (obbligatorio): ");
+            int capienzaMassima = int.Parse(Console.ReadLine());
+
+            Evento evento = new Evento(titolo, data, capienzaMassima);
+
             
         }
     }
