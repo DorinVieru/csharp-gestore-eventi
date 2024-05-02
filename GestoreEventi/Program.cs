d2a1a99d2556486b8cab2f20bf24963074f8c17f
DorinVieru <dorino.vieru@gmail.com>
2024-05-02 11:47:05 +0200

    UP: Write system of reserve seats

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Inizia a gestire subito i tuoi eventi meravigliosi!");
 
             // Creazione di un nuovo evento
-            Console.Write("Inserisci il nome dell'evento (obbligatorio): ");
+            Console.Write("\n******************\nInserisci il nome dell'evento (obbligatorio): ");
             string titolo = Console.ReadLine();
             Console.Write("Inserisci la data dell'evento (obbligatorio formato dd/MM/yyyy): ");
             DateTime data = DateTime.Parse(Console.ReadLine());
@@ -16,7 +16,28 @@
 
             Evento evento = new Evento(titolo, data, capienzaMassima);
 
-            
+            // Prenotazione dei posti
+            Console.WriteLine("******************\n\nDesideri prenotare dei posti? (digita si / no)");
+            string prenotazione = Console.ReadLine().ToLower();
+            while (prenotazione == "si")
+            {
+                Console.Write("Quanti posti vuoi prenotare? ");
+                int numPosti = int.Parse(Console.ReadLine());
+                try
+                {
+                    evento.PrenotaPosti(numPosti);
+                    Console.WriteLine($"Hai prenotato {numPosti} posti. \nPosti prenotati - {evento.PostiPrenotati} \nPosti disponibili - {evento.CapienzaMassima - evento.PostiPrenotati}");
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+
+                Console.WriteLine("\nDesideri prenotare ancora dei posti? (digita si / no)");
+                prenotazione = Console.ReadLine().ToLower();
+            }
+
+         
         }
     }
 }
