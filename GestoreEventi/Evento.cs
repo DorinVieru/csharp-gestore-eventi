73eaa7e42ac279c2af3eaef86e9d064328dd15cf
DorinVieru <dorino.vieru@gmail.com>
2024-05-02 10:15:14 +0200

    UP: Write attribute & constructur of class Evento

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        // Attributi classe evento
+        private string titolo;
+        private DateTime data;
+        private int capienzaMassima;
+        private int postiPrenotati;
+
+        // Costruttore
+        public Evento(string titolo, DateTime data, int capienzaMassima)
+        {
+            this.Titolo = titolo;
+            this.Data = data;
+            this.CapienzaMassima = capienzaMassima;
+            this.PostiPrenotati = 0; 
+        }
+
+        
+    }
+}
