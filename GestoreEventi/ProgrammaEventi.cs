b43f3161fe8b3edd73e8b3726d0d7983b96b030a
DorinVieru <dorino.vieru@gmail.com>
2024-05-02 12:26:31 +0200

    UP: Write class ProgrammaEventi with its attributes & constructur

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class ProgrammaEventi
+    {
+        // Attributi per ProgrammaEventi
+        private string titolo;
+        private List<Evento> eventi;
+
+        // Costruttore
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            this.eventi = new List<Evento>();
+        }
+
+        
+    }
+}
